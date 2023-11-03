70fd17627376e2afc946610e34838414e8ef9007
Matthew Dorsey <madorse2@ncsu.edu>
2023-11-03 13:59:42 -0400

    Added initial fileio file with pseudocode for Florian.

---
+++ fortran/simlib/class_fileio/fileio.f90
@@ -0,0 +1,18 @@
+! AUTHOR : 
+! DATE : 
+! FILENAME : 
+! DESCRIPTION : 
+
+module class_fileio
+
+    ! define fileio class
+    type filio 
+        ! list  datatypes that belong to object
+        ! TODO :: add instance variables
+        end type filio
+
+    contains
+
+    ! TODO :: write pseudocode for methods within
+
+	end module class_fileio
